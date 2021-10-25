afec7b1e3eef77ca71ec5edb5ef175a220b04890
merechko.dmitriy <merechko.dmitriy@otr.ru>
2021-10-25 14:06:26 +0300

    main container Quiz

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import {Component} from "react";
 import Layout from "./hoc/Layout/Layout";
+import Quiz from "./containers/Quiz/Quiz";
 
 class App extends Component {
     render() {
         return (
             <Layout>
-                <div style={{border: '1px solid #ccc', width: 400, margin: '0 auto'}}>
-                    <h1>Layout</h1>
-                </div>
+                <Quiz/>
             </Layout>
         )
     }
