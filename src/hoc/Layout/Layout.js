1dbf684f3e1bd0e9ae79a5f0c96a78fb93e76cd8
merechko.dmitriy <merechko.dmitriy@otr.ru>
2021-10-26 17:23:45 +0300

    cherry-pick

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,39 @@
 import {Component} from "react";
 import classes from './Layout.module.css'
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
+import Drawer from "../../components/Navigation/Drawer/Drawer";
 
 class Layout extends Component {
+
+    state = {
+        menu: false
+    }
+
+    onToggleHandler = () => {
+        this.setState({
+            menu: !this.state.menu
+        })
+    }
+
+    onCloseHandler = () => {
+        this.setState({
+            menu: false
+        })
+    }
+
     render() {
         return (
             <div className={classes.Layout}>
+
+                <Drawer
+                    isOpen={this.state.menu}
+                    onClose={this.onCloseHandler}
+                />
+                <MenuToggle
+                    isOpen={this.state.menu}
+                    onToggle={this.onToggleHandler}
+                />
+
                 <main>
                     {this.props.children}
                 </main>
